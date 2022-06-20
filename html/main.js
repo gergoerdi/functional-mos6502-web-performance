004a67680db9786f79d3f27148ca143ab4843e80
Gergo ERDI <gergo@erdi.hu>
2022-06-20 20:27:39 +0800

    Measure min/max/avg of various implementations

--- html/main.js
+++ html/main.js
@@ -1,25 +1,59 @@
-var purs;
-var js;
+let implementations = {};
 
-let measure = (label, act) => {
-    let before = +new Date();
-    let cnt = act();
-    let after = +new Date();
-    console.log(label + ": " + cnt + " cycles done in " + (after - before) + "ms");
+function measure(label, act) {
+    const before = +new Date();
+    const cnt = act();
+    const after = +new Date();
+    if (cnt != 4142) throw { label: label, cnt: cnt };
+
+    const time = after - before;
+    // console.log(label + ": " + cnt + " cycles done in " + time + "ms");
+    return time;
 };
 
-import('./implementations/purescript/bundle.js').then(mod => {
-    purs = () => {
-        measure("PureScript", mod.run(fn => () => files[fn].slice()));
-    };
-});
+function measureAll() {
+    const numRuns = 100;
+    const numWarmup = 10;
 
-let idris2 = () => {
-    measure("Idris2", () => idris2_run(fn => w => files[fn].slice()));
+    for (const [label, act] of Object.entries(implementations)) {
+        for (let i = 0; i < numWarmup; ++i) {
+            measure(label, act);
+        }
+
+        let times = [];
+        for (let i = 0; i < numRuns; ++i) {
+            times.push(measure(label, act));
+        }
+
+        let minTime = null, sumTime = 0, maxTime = null;
+        for (const time of times) {
+            minTime = !minTime || time < minTime ? time : minTime;
+            maxTime = !maxTime || time > maxTime ? time : maxTime;
+            sumTime += time;
+        }
+
+        const avgTime = sumTime / numRuns;
+
+        console.log(label + ":" +
+                    " min: " + minTime + "ms" +
+                    " max: " + maxTime + "ms" +
+                    " avg: " + avgTime + "ms");
+    }
 }
 
-import('./implementations/js/mos6502.js').then(mod => {
-    js = () => {
-        measure("Javascript", mod.run(fn => () => files[fn].slice()));
-    };
-});
+async function setup()
+{
+    {
+        const mod = await import("./implementations/purescript/bundle.js");
+        implementations["PureScript"] = () => mod.run(fn => () => files[fn].slice())();
+    }
+
+    implementations["Idris2"] = () => idris2_run(fn => w => files[fn].slice());
+
+    {
+        const mod = await import("./implementations/js/mos6502.js");
+        implementations["JavaScript"] = () => mod.run(fn => () => files[fn].slice())();
+    }
+}
+
+setup().then({});

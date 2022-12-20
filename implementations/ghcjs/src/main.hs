f1251bbfa4f400e8e596ecba80681afbf8ee304f
Gergo ERDI <gergo@erdi.hu>
2022-12-20 22:22:11 +0800

    Instead of writing into a preallocated buffer, have `loadFile` return a new one

--- implementations/ghcjs/src/main.hs
+++ implementations/ghcjs/src/main.hs
@@ -6,22 +6,19 @@ import Driver
 import Foreign.C.String
 import Foreign.C.Types
 import Foreign.Ptr
-import Foreign.Storable
-import Foreign.Marshal.Alloc
 
 import qualified Data.ByteString as BS
 
-foreign import javascript "((p,o,n,p2) => loadFile(h$decodeUtf8(p,n,o),p2))" loadFile_ :: Ptr CChar -> Int -> Ptr CChar -> IO ()
+foreign import javascript "((p,o,n) => loadFile(h$decodeUtf8(p,n,o)))" loadFile_ :: Ptr CChar -> Int -> IO (Ptr CChar)
 foreign import javascript "((p) => p.len)" bufferLength_ :: Ptr CChar -> IO Int
 
-loadFile :: String -> IO BS.ByteString
+loadFile :: String -> IO CStringLen
 loadFile fileName = withCStringLen fileName \(ptr, len) -> do
-    allocaBytes 100000 \ptr' -> do
-        loadFile_ ptr len ptr'
-        len' <- bufferLength_ ptr'
-        BS.packCStringLen (ptr', len')
+    ptr' <- loadFile_ ptr len
+    len' <- bufferLength_ ptr'
+    return (ptr', len')
 
 main :: IO ()
 main = do
-    bs <- loadFile "data/program.dat"
+    bs <- BS.packCStringLen =<< loadFile "data/program.dat"
     print =<< run bs
--- implementations/ghcjs/top.js
+++ implementations/ghcjs/top.js
@@ -1,11 +1,7 @@
-function loadFile(s, target) {
+function loadFile(s) {
     console.log("loadFile: " + s);
     let buf = files[s].slice();
 
-    // return h$wrapBuffer(buf, true, null, null);
-
-    target.len = buf.byteLength;
-    let buf8 = new Uint8Array(buf);
-    for (var i = 0; i < buf8.length; ++i)
-        target.u8[i] = buf8[i];
+    h$ret1 = 0;
+    return h$wrapBuffer(buf, true, null, null);
 }
